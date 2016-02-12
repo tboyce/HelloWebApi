9c98659dd6811bd0aa1101d7b09ccd576d952768
Tim Boyce <timothy.boyce@mmgins.com>
2016-02-12 11:11:56 -0500

    initial commit

---
+++ HelloWebApi/Controllers/GreetingsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace HelloWebApi.Controllers
+{
+    public class GreetingsController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok(new[] {"Hello world"});
+        }
+    }
+}
---
+++ HelloWebApi/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+using Owin;
+
+namespace HelloWebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var configuration = new HttpConfiguration();
+
+            configuration.Routes.MapHttpRoute(
+                name: "Default Route",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new {id = RouteParameter.Optional}
+                );
+
+
+            appBuilder.UseWebApi(configuration);
+        }
+    }
+}
