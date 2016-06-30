a9e69b860ab880f7565a75a4fe0d34f78f847031
Timothy Boyce <timothy.boyce@mmgins.com>
2016-06-29 23:16:35 -0400

    add enterprise library

--- HelloWebApi/ErrorHandling/ErrorMessageExceptionHandler.cs
+++ HelloWebApi/ErrorHandling/ErrorMessageExceptionHandler.cs
@@ -1,6 +1,9 @@
-﻿using System.Threading;
+﻿using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http.ExceptionHandling;
+using Microsoft.Practices.EnterpriseLibrary.ExceptionHandling;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
 
 namespace HelloWebApi.ErrorHandling
 {
@@ -9,12 +12,23 @@ namespace HelloWebApi.ErrorHandling
     /// </summary>
     public class ErrorMessageExceptionHandler : ExceptionHandler
     {
-        public override void Handle(ExceptionHandlerContext context)
+        public ErrorMessageExceptionHandler()
         {
+            Logger.SetLogWriter(new LogWriterFactory().Create());
+            ExceptionPolicy.SetExceptionManager(new ExceptionPolicyFactory().CreateManager());
         }
+       
 
         public override Task HandleAsync(ExceptionHandlerContext context, CancellationToken cancellationToken)
         {
+            var exception = context.Exception;
+            if (!exception.Data.Contains("RequestId"))
+            {
+                exception.Data.Add("RequestId", context.Request.GetCorrelationId());
+            }
+
+            ExceptionPolicy.HandleException(context.Exception, "Logging Policy");
+
             context.Result = new ErrorMessage(context.Request);
 
             return base.HandleAsync(context, cancellationToken);
